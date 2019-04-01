16e03430937d4102c6f8040c4e486cddb1f21db3
kishansheth <kishansheth98@gmail.com>
2019-04-01 17:56:46 -0400

    Initial commit on draft branch

---
+++ signafire-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {MessageViewer} from './pages';
+
+class App extends Component {
+  render() {
+    return (
+      <MessageViewer/>
+    );
+  }
+}
+
+export default App;
---
+++ signafire-app/src/_styles/theme.js
@@ -0,0 +1,37 @@
+const theme = {
+  colors: {
+    main: '#AAAAAA',
+  },
+  font: {
+    colors: {
+      dark: '#000000',
+    },
+    sizes: {
+      h1: '3.052em',
+      h2: '2.441em',
+      h3: '1.953em',
+      h4: '1.563em',
+      h5: '1.25em',
+      p:  '.75em',
+    },
+    weights: {
+      thin: '100',
+      light: '300',
+      regular: '400',
+      bold: '700',
+      black: '800',
+    }
+  },
+  border: {
+    colors: {
+      dark: '#AAAAAA',
+    },
+  },
+  spacing: {
+    huge: '50px',
+    big: '20px',
+    small: '10px',
+  },
+};
+
+export {theme};
