b64cb2d18f8e61dd1f390ad23fca640e1e431ce7
kishansheth <kishansheth98@gmail.com>
2019-04-02 03:44:50 -0400

    Added profile image, handle, and text to messages

--- signafire-app/src/components/Message.js
+++ signafire-app/src/components/Message.js
@@ -5,30 +5,69 @@ import { theme } from '../_styles';
 const MessageContainer = styled.div`
     display: flex;
     box-sizing: border-box;
-    flex-direction: column;
     border: 1px solid ${theme.colors.outline};
     padding: 20px;
     margin-top: 20px;
     margin-bottom: 20px;
+`;
 
-    .profile {
-
+const Profile = styled.div`
+    width: 15%;
+    text-align: center;
+    padding: 10px;
+    .avatar {
+        width: 150px;
     }
-
-    .content {
-
-    }
-
-    .sections {
-        
+    .name {
+        padding: 10px;
     }
 `;
 
+const Content = styled.div`
+    width: 75%;
+    text-align: left;
+    .metadata {
+        padding: 10px;
+        font-size: ${theme.font.sizes.p};
+        color: ${theme.font.colors.light};
+    }
+    .message_body {
+        padding: 10px;
+        color: ${theme.font.colors.dark};
+    }
+`;
+
+const Buttons = styled.div`
+    width: 15%;
+`;
+
 class Message extends React.Component {
+    constructor(props) {
+        super(props);
+
+        var d = new Date(this.props.content.timestamp);
+        var date_string = d.toLocaleString('en-US', {month: 'short'}) + " " + d.getDate() + ', ' + d.getFullYear(); 
+
+        this.state = {
+            date:date_string,
+            starred:this.props.content.meta.isStarred,
+        };
+    }
+    
     render() {
         return (
             <MessageContainer>
-                {this.props.content.handle}
+                <Profile>
+                    <img className={'avatar'} src={this.props.content.avatar}/>
+                    <div className={'name'}>{this.props.content.handle}</div>
+                </Profile>
+                <Content>
+                    <div className={'metadata'}>{this.props.content.source} | {this.state.date}</div>
+                    <div className={'message_body'}>{this.props.content.content}</div>
+                </Content>
+                <Buttons>
+                
+                </Buttons>
             </MessageContainer>
         )
     }
